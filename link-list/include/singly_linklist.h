4ed2542325bd8a8d08d833618301bb71f659aba4
hieunq <hieu811@gmail.com>
2021-05-04 16:22:14 +0700

    add method for linknlist

--- link-list/include/singly_linklist.h
+++ link-list/include/singly_linklist.h
@@ -15,7 +15,10 @@ struct LinkListNode {
 
     LinkListNode(T data) {
        this->data = data;
-       this->next = nullptr;
+       this->next = NULL;
+    }
+    LinkListNode(){
+        this->next = NULL;
     }
 };
 
@@ -25,33 +28,78 @@ using LinklistCallback = void (*)(T);
 template<typename T>
 class SinglyLinkList {
 public:
-    SinglyLinkList(){}
+    SinglyLinkList(){
+        start = new LinkListNode<T>();
+    }
     ~SinglyLinkList();
 
 public:
     void addData(T data){
-        LinkListNode<T>* node = new LinkListNode<T>(data);
-        if(start == nullptr) {
-            start = node;
-            return;
-        }
+        auto node = new LinkListNode<T>(data);
 
         LinkListNode<T>* cur = start;
-        while (cur->next != nullptr) {
+        while (cur->next != NULL) {
             cur = cur->next;
         };
         cur->next = node;
     }
-    void traversalNode(LinklistCallback<T> cb) {
-        LinkListNode<T> *currentNode = start;
-        do {
+    void foreach(LinklistCallback<T> cb) {
+        LinkListNode<T> *currentNode = start->next;
+        while (currentNode != NULL) {
             cb(currentNode->data);
             currentNode = currentNode->next;
         }
-        while (currentNode != nullptr);
+    }
+
+    LinkListNode<T>* deleteNode(int idx) {
+        int curIdx = 0;
+        LinkListNode<T> *prev = start;
+        LinkListNode<T> *cur = prev->next;
+        while (cur != NULL){
+            if(curIdx == idx) {
+                prev->next = cur->next;
+                cur->next = NULL;
+                return cur;
+            }
+
+            prev = cur;
+            cur = cur->next;
+            curIdx++;
+        }
+
+        return nullptr;
+    }
+
+    int length(){
+        int curIdx = 0;
+        LinkListNode<T> *currentNode = start->next;
+        while (currentNode != NULL) {
+            curIdx++;
+            currentNode = currentNode->next;
+        }
+        return curIdx;
+    }
+
+    void findIndex(int idx, LinklistCallback<T> cb) {
+        int curIdx = 0;
+        LinkListNode<T> *currentNode = start->next;
+        while (currentNode != NULL) {
+            if(curIdx == idx) {
+                cb(currentNode->data);
+            }
+            curIdx++;
+            currentNode = currentNode->next;
+        }
+    }
+
+    void deleteList(){
+        int len = this->length();
+        for(int i = len - 1; i >= 0; i--){
+            this->deleteNode(i);
+        }
     }
 
 private:
-    LinkListNode<T> *start = nullptr;
+    LinkListNode<T> *start = NULL;
 };
 #endif //ALGORITHM_AND_DATA_STRUCTURE_SINGLY_LINKLIST_H
--- link-list/src/main.cpp
+++ link-list/src/main.cpp
@@ -6,12 +6,30 @@
 
 int main(){
     auto intList = new SinglyLinkList<int>();
+    int idx = 2;
     intList->addData(1);
     intList->addData(2);
     intList->addData(3);
+    intList->addData(4);
 
-    intList->traversalNode([](int data){
+    printf("length: %d\n", intList->length());
+    intList->foreach([](int data){
        printf("%d\n", data);
+    });
+    intList->findIndex(idx, [](int data){
+        printf("found data %d\n", data);
+    });
+
+    intList->deleteNode(idx);
+    printf("length after delete: %d\n", intList->length());
+    intList->foreach([](int data){
+        printf("%d\n", data);
+    });
+
+    intList->deleteList();
+    printf("length after delete: %d\n", intList->length());
+    intList->foreach([](int data){
+        printf("%d\n", data);
     });
     return 0;
 }
